25b5a1aa647b65c7f5dba876aab30475b1b8a106
nelyjc <nely.jazmin@outlook.com>
2025-03-16 18:58:35 -0600

    entry done

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -8,7 +8,7 @@ public class Entry
 
     public Entry(string prompt, string response)
     {
-        Date = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        Date = DateTime.Now.ToString("MM-dd-yyyy HH:mm");
         Prompt = prompt;
         Response = response;
     }
@@ -18,6 +18,6 @@ public class Entry
         Console.WriteLine($"\nDate: {Date}");
         Console.WriteLine($"Prompt: {Prompt}");
         Console.WriteLine($"Response: {Response}");
-        Console.WriteLine(new string('-', 30));
+        Console.WriteLine(new string('-', 20));
     }
 }
