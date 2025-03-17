ebefbe44bd8a24e957a8473e444fdc0c959f9e20
nelyjc <nely.jazmin@outlook.com>
2025-03-16 18:48:53 -0600

    trying 2

---
+++ week02/Journal/Entry.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Entry
+{
+    public string Date { get; set; }
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+
+    public Entry(string prompt, string response)
+    {
+        Date = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        Prompt = prompt;
+        Response = response;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"\nDate: {Date}");
+        Console.WriteLine($"Prompt: {Prompt}");
+        Console.WriteLine($"Response: {Response}");
+        Console.WriteLine(new string('-', 30));
+    }
+}
