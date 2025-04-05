61d1339b8ed48e66860faa8bf0fd9402fdd7032f
nelyjc <nely.jazmin@outlook.com>
2025-04-05 11:51:14 -0600

    complete

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        MathAssignment MathHomework = new MathAssignment("Samuel Bennett", "Multiplication", "7.3", "8-19");;
+
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine(MathHomework.GetHomeworkList());
+        
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
+        
     }
 }
